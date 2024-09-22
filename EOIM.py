d3a90c165cdb8e4aaf44f4087626a6bdaa8442c4
fadmar <mark.fadeev2017@yandex.ru>
2024-09-22 13:35:40 +0300

    EOIM update

--- EOIM.py
+++ EOIM.py
@@ -1,8 +1,6 @@
 import math
 from sympy import *
 import numexpr
-from sympy.core.evalf import evalf_float
-
 from Error import Error
 
 
@@ -12,10 +10,7 @@ class EOIM:  # Error_of_indirect_measurements
         self.all_inst_err = all_inst_err
         self.formula = formula
 
-    def calculate(self):
-        s = ''
-
-    def mfs(self):  # method for sum
+    def mth(self, meth):  # method for sum
         er1 = Error(self.allmsm[0], self.all_inst_err[0])
         er2 = Error(self.allmsm[1], self.all_inst_err[1])
         er3 = Error(self.allmsm[2], self.all_inst_err[2])
@@ -29,26 +24,50 @@ class EOIM:  # Error_of_indirect_measurements
         a, b, c, d, e, f, g, h, i, j = symbols("a, b, c, d, e, f, g, h, i, j")
         dic = {a: er1.average_x(), b: er2.average_x(), c: er3.average_x(), d: er4.average_x(), e: er5.average_x(),
                f: er6.average_x(), g: er7.average_x(), h: er8.average_x(), i: er9.average_x(), j: er10.average_x()}
-        s = 0
         symp_formula = sympify(self.formula)
         z_average = symp_formula.evalf(subs=dic)  # formula(variant) = "a*b"
-        s += (diff(symp_formula, a).evalf(subs=dic) * er1.absolute_error()) ** 2
-        s += (diff(symp_formula, b).evalf(subs=dic) * er2.absolute_error()) ** 2
-        s += (diff(symp_formula, c).evalf(subs=dic) * er3.absolute_error()) ** 2
-        s += (diff(symp_formula, d).evalf(subs=dic) * er4.absolute_error()) ** 2
-        s += (diff(symp_formula, e).evalf(subs=dic) * er5.absolute_error()) ** 2
-        s += (diff(symp_formula, f).evalf(subs=dic) * er6.absolute_error()) ** 2
-        s += (diff(symp_formula, g).evalf(subs=dic) * er7.absolute_error()) ** 2
-        s += (diff(symp_formula, h).evalf(subs=dic) * er8.absolute_error()) ** 2
-        s += (diff(symp_formula, i).evalf(subs=dic) * er9.absolute_error()) ** 2
-        s += (diff(symp_formula, j).evalf(subs=dic) * er10.absolute_error()) ** 2
-        ind_absol_err = sqrt(s)
-        return ind_absol_err
+
+        def mfs():
+            s = 0
+            s += (diff(symp_formula, a).evalf(subs=dic) * er1.absolute_error()) ** 2
+            s += (diff(symp_formula, b).evalf(subs=dic) * er2.absolute_error()) ** 2
+            s += (diff(symp_formula, c).evalf(subs=dic) * er3.absolute_error()) ** 2
+            s += (diff(symp_formula, d).evalf(subs=dic) * er4.absolute_error()) ** 2
+            s += (diff(symp_formula, e).evalf(subs=dic) * er5.absolute_error()) ** 2
+            s += (diff(symp_formula, f).evalf(subs=dic) * er6.absolute_error()) ** 2
+            s += (diff(symp_formula, g).evalf(subs=dic) * er7.absolute_error()) ** 2
+            s += (diff(symp_formula, h).evalf(subs=dic) * er8.absolute_error()) ** 2
+            s += (diff(symp_formula, i).evalf(subs=dic) * er9.absolute_error()) ** 2
+            s += (diff(symp_formula, j).evalf(subs=dic) * er10.absolute_error()) ** 2
+            ind_absol_err = sqrt(s)
+            ind_relative_err = (ind_absol_err / z_average) * 100
+            return ind_relative_err
+
+        def mfp():
+            s = 0
+            symp_formula2 = sympify("ln(" + self.formula + ")")
+            s += (diff(symp_formula2, a).evalf(subs=dic) * er1.absolute_error()) ** 2
+            s += (diff(symp_formula2, b).evalf(subs=dic) * er2.absolute_error()) ** 2
+            s += (diff(symp_formula2, c).evalf(subs=dic) * er3.absolute_error()) ** 2
+            s += (diff(symp_formula2, d).evalf(subs=dic) * er4.absolute_error()) ** 2
+            s += (diff(symp_formula2, e).evalf(subs=dic) * er5.absolute_error()) ** 2
+            s += (diff(symp_formula2, f).evalf(subs=dic) * er6.absolute_error()) ** 2
+            s += (diff(symp_formula2, g).evalf(subs=dic) * er7.absolute_error()) ** 2
+            s += (diff(symp_formula2, h).evalf(subs=dic) * er8.absolute_error()) ** 2
+            s += (diff(symp_formula2, i).evalf(subs=dic) * er9.absolute_error()) ** 2
+            s += (diff(symp_formula2, j).evalf(subs=dic) * er10.absolute_error()) ** 2
+            ind_relative_err = sqrt(s) * 100
+            ind_absol_err = z_average * ind_relative_err / 100
+            return ind_relative_err
+
+        if meth == "0":
+            return mfs()
+        else:
+            return mfp()
 
 
-m = [[1, 1, 1, 1, 1], [2, 2, 2, 2, 2], [0], [0], [0], [0], [0], [0], [0], [0]]
-ins = [1, 1, 0, 0, 0, 0, 0, 0, 0, 0]
-er11 = Error(m[0], 1)
-er22 = Error(m[1], 1)
-eoim = EOIM(m, ins, "(a)*(b)")
-print(eoim.mfs())
+m = [[1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+     [2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], [0], [0], [0], [0], [0], [0], [0], [0]]
+ins = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+eoim = EOIM(m, ins, "(a)+(b)")
+print(eoim.mth('0'))
