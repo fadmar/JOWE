5cab40fe65b9a73e8c9fcaceba38719971bc1c0a
fadmar <mark.fadeev2017@yandex.ru>
2024-09-22 15:49:40 +0300

    readme update

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from EOIM import EOIM
-from Error import Error
-import Data
-import Inst
-# example
-#dat = Data.Data(2)
-#ins = Inst.Inst()
+# example1
+from Data import Data
+from Inst import Inst
+#dat = Data(2)
+#inst = Inst()
+# example2
 #from Error import Error
 #err = Error([0, 1, 1], 0)
 #print(err.absolute_error())
-from EOIM import EOIM
-eoim = EOIM(dat.data(), ins.inst(), "a+b**2")
-print(eoim.mth("0","rel"))
+# example3
+# from EOIM import EOIM
+# eoim = EOIM(dat.data(), inst.inst(), "a+b**2")
+# print(eoim.mth("0","rel"))
