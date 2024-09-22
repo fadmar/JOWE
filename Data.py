49162976781436598f44eed0276530da4ac535c8
fadmar <mark.fadeev2017@yandex.ru>
2024-09-22 12:00:41 +0300

    EOIM update

--- Data.py
+++ Data.py
@@ -4,11 +4,11 @@ class Data:
         self.number_of_measurements = number_of_measurements  # количество измерений
 
     def data(self):
-        MSRM = [0] * self.number_of_measurements
+        allmsm = [0] * self.number_of_measurements
         for i in range(self.number_of_measurements):
             with open('Data/data' + str(i + 1) + '.txt', 'r') as f:
                 x = f.read().splitlines()
                 for j in range(len(x)):
                     x[j] = float(x[j])
-            MSRM[i] = x
-        return MSRM
+            allmsm[i] = x
+        return allmsm
--- EOIM.py
+++ EOIM.py
@@ -1,27 +1,40 @@
 import math
 from sympy import *
 import numexpr
+from sympy.core.evalf import evalf_float
 
 from Error import Error
 
 
 class EOIM:  # Error_of_indirect_measurements
-    def __init__(self, arr, formula):   #formula = "x[0]*x[1]**(-2)"
-        self.arr = arr
+    def __init__(self, allmsm, all_inst_err, formula):
+        self.allmsm = allmsm
+        self.all_inst_err = all_inst_err
         self.formula = formula
 
     def calculate(self):
         s = ''
 
-    def mfs(self, arr_of_variable):  # method for sum
-        x = arr_of_variable
-        z = eval(self.formula)
-        return z
+    def mfs(self):  # method for sum
+        er1 = Error(self.allmsm[0], self.all_inst_err[0])
+        er2 = Error(self.allmsm[1], self.all_inst_err[0])
+        a = er1.average_x()
+        b = er2.average_x()
+        a, b = symbols("a, b")
+        dic = {a: er1.average_x(), b: er2.average_x()}
+        z_average = eval(self.formula)  # formula(variant) = "a*b"
+        s = 0
+        symp_formula = sympify(self.formula)
+        s += (diff(symp_formula, a).evalf(subs=dic) * er1.absolute_error()) ** 2
+        s += (diff(symp_formula, b).evalf(subs=dic) * er2.absolute_error()) ** 2
+
+        ind_absol_err = sqrt(s)
+        return ind_absol_err
 
 
-
-m = [[1,1,1,1,1],[2,2,2,2,2]]
-er1 = Error([1,1,1,1,1], 1)
-er2 = Error([2,2,2,2,2], 1)
-eoim = EOIM(m,"x[0]*x[1]**(-2)" )
-print(eoim.mfs([er1.average_x(),er2.average_x()]))
+m = [[1, 1, 1, 1, 1], [2, 2, 2, 2, 2]]
+ins = [1, 1]
+er11 = Error(m[0], 1)
+er22 = Error(m[1], 1)
+eoim = EOIM(m, ins, "(a)*(b)")
+print(eoim.mfs())
